c0bd4158e988cd9a78e60144bcef0fc610c14ac6
hernikplays <25116608+hernikplays@users.noreply.github.com>
2018-03-29 19:30:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var apis = {
 };
 
 //Replace this with your status page project name
-var githubProject = 'bisscomm/status';
+var githubProject = 'hernikplays/hernikplays-website';
 
 //Github URLs
 var issuesApiUrl = 'https://api.github.com/repos/'+githubProject+'/issues?limit=100&sort=created&direction=desc&state=all'
